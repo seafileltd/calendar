22d15b6b679727735301567bc15502c0bf6613e6
GongFlying <150640661+gzcqqqqqqqq1@users.noreply.github.com>
2025-05-28 13:44:20 +0800

    Time algorithm adjustment

--- src/date/DateInput.js
+++ src/date/DateInput.js
@@ -4,11 +4,7 @@ import PropTypes from 'prop-types';
 import KeyCode from 'rc-util/lib/KeyCode';
 import { polyfill } from 'react-lifecycles-compat';
 import moment from 'moment';
-import {
-  formatDate, DATE_FORMATS, currentDate, currentMonth, currentYear,
-  stringCurrentDate, stringCurrentMonth, formatDateLocal, hasSpecialChar,
-  isValidMonth, isValidDay, fullValidYear, getDateFormatByStr, isCurrentYear,
-} from '../util';
+import { formatDate, DATE_FORMATS } from '../util';
 
 let cachedSelectionStart;
 let cachedSelectionEnd;
@@ -30,19 +26,18 @@ class DateInput extends React.Component {
     selectedValue: PropTypes.object,
     clearIcon: PropTypes.node,
     inputMode: PropTypes.string,
-    showHourAndMinute: PropTypes.bool,
     onChangeCananderIput: PropTypes.func,
   }
 
   constructor(props) {
     super(props);
     const selectedValue = props.selectedValue;
-
-    const formatPrefix = this.props.format[0].slice(0, this.props.format[0].indexOf(' '));
+    const formatPrefix = this.props.format[0];
     let delimiter;
-    if (formatPrefix === DATE_FORMATS.Germany_Russia_etc) {
+    if (formatPrefix === DATE_FORMATS.Germany_Russia_etc ||
+      formatPrefix === DATE_FORMATS.Germany_Russia_etcAndTime) {
       delimiter = '.';
-    } else if (formatPrefix === DATE_FORMATS.ISO) {
+    } else if (formatPrefix === DATE_FORMATS.ISO || formatPrefix === DATE_FORMATS.ISOAndTime) {
       delimiter = '-';
     } else {
       delimiter = '/';
@@ -73,11 +68,9 @@ class DateInput extends React.Component {
 
   onInputChange = (event) => {
     const str = event.target.value;
-    const { disabledDate, showHourAndMinute, onChange, selectedValue } = this.props;
-    const cananderStr = this.normalizeDateInput(str, showHourAndMinute);
-    const parts = formatDateLocal(cananderStr, this.state.localeFormat);
+    const { disabledDate, format, onChange, selectedValue } = this.props;
 
-    this.setState({ cananderIput: cananderStr });
+    this.setState({ cananderIput: str });
     // 没有内容，合法并直接退出
     if (!str) {
       onChange(null);
@@ -86,11 +79,7 @@ class DateInput extends React.Component {
     }
 
     // 不合法直接退出
-    let format = getDateFormatByStr(cananderStr, this.state.localeFormat);
-    if (/\d{1,2}:\d{2}$/.test(cananderStr) && showHourAndMinute) {
-      format += ' HH:mm';
-    }
-    const parsed = moment(cananderStr, format);
+    const parsed = moment(str, format);
 
     let value = this.props.value.clone();
     value = value
@@ -100,21 +89,6 @@ class DateInput extends React.Component {
       .hour(parsed.hour() || moment().hour())
       .minute(parsed.minute() || moment().minute())
       .second(parsed.second() || moment().second());
-    if (this.state.localeFormat === DATE_FORMATS.ISO) {
-      if (parts[0] && parts[0].length === 4 && (parts[0].slice(0, 3) === '000' ||
-        parts[0].slice(0, 2) === '00')) {
-        value.year(parts[0]);
-      }
-    }
-    if (this.state.localeFormat === DATE_FORMATS.European ||
-        this.state.localeFormat === DATE_FORMATS.US ||
-        this.state.localeFormat === DATE_FORMATS.Germany_Russia_etc
-    ) {
-      if (parts[2] && parts[2].length === 4 && (parts[2].slice(0, 3) === '000' ||
-        parts[2].slice(0, 2) === '00')) {
-        value.year(parts[2]);
-      }
-    }
     if (!value || (disabledDate && disabledDate(value))) {
       this.setState({ str });
       return;
@@ -165,19 +139,30 @@ class DateInput extends React.Component {
       const parts = timeStr.split(state.delimiter);
       const timeParts = formatDate(selectedValue, nextProps.format).split(' ')[1];
       if (state.localeFormat === DATE_FORMATS.ISO) {
-        if (parts[0].length > 0) {
-          newState = { str: `${parts[0].padStart(4, 0)}-${parts[1]}-${parts[2]} ${timeParts}` };
+        if (parts.length === 3) {
+          newState = { str: `${parts[0].padStart(4, 0)}-${parts[1]}-${parts[2]}` };
+        }
+      } else if (state.localeFormat === DATE_FORMATS.ISOAndTime) {
+        if (parts.length === 3) {
+          newState = { str: `${parts[0].padStart(4, 0)}-${parts[1]}-${parts[2]} ${timeParts && timeParts}` };// eslint-disable-line max-len
         }
       } else if (state.localeFormat === DATE_FORMATS.US) {
         if (parts.length === 3) {
-          newState = { str: `${Number(parts[0])}/${Number(parts[1])}
-          /${parts[2].padStart(4, 0)} ${timeParts}` };
+          newState = { str: `${Number(parts[0])}/${Number(parts[1])}/${parts[2].padStart(4, 0)}` };
+        }
+      } else if (state.localeFormat === DATE_FORMATS.USAndTime) {
+        if (parts.length === 3) {
+          newState = { str: `${Number(parts[0])}/${Number(parts[1])}/${parts[2].padStart(4, 0)} ${timeParts && timeParts}` };// eslint-disable-line max-len
         }
       } else if (state.localeFormat === DATE_FORMATS.European ||
         state.localeFormat === DATE_FORMATS.Germany_Russia_etc) {
         if (parts.length === 3) {
-          newState = { str: `${Number(parts[0])}${state.delimiter}${Number(parts[1])}
-          ${state.delimiter}${parts[2].padStart(4, 0)} ${timeParts}` };
+          newState = { str: `${Number(parts[0])}${state.delimiter}${Number(parts[1])}${state.delimiter}${parts[2].padStart(4, 0)}` };// eslint-disable-line max-len
+        }
+      } else if (state.localeFormat === DATE_FORMATS.EuropeanAndTime ||
+          state.localeFormat === DATE_FORMATS.Germany_Russia_etcAndTime) {
+        if (parts.length === 3) {
+          newState = { str: `${Number(parts[0])}${state.delimiter}${Number(parts[1])}${state.delimiter}${parts[2].padStart(4, 0)} ${timeParts && timeParts}` };// eslint-disable-line max-len
         }
       }
     }
@@ -190,145 +175,6 @@ class DateInput extends React.Component {
 
   getRootDOMNode = () => {
     return ReactDOM.findDOMNode(this);
-  }
-
-  normalizeDateInput(str, showHourAndMinute) {
-    let day;
-    let month;
-    let year;
-    const parts = formatDateLocal(str, this.state.localeFormat, DATE_FORMATS);
-    const delimiter = this.state.delimiter;
-    const hasSpecial = hasSpecialChar(str);
-    if (this.state.localeFormat === DATE_FORMATS.ISO) {
-      const numStr = str.replace(/[^0-9]/g, '');
-      if (numStr.length === 7) {
-        year = numStr.slice(0, 4);
-        month = numStr.slice(4, 6).padStart(2, '0');
-        day = numStr.slice(6, 7).padStart(2, '0');
-        if (!isValidDay(day)) {
-          return `${year}-${stringCurrentMonth}-
-          ${stringCurrentDate} ${showHourAndMinute && `${parts[3]}:${parts[4]}`}`;
-        }
-        return `${year}-${month}-${day}`;
-      }
-      if (hasSpecial) {
-        year = fullValidYear(parts[0]);
-        month = Number(parts[1]);
-        day = Number(parts[2]);
-        if (month >= 1 && month <= 12) {
-          if (isValidDay(day)) {
-            return `${year}-${String(month).padStart(2, '0')}-
-            ${String(day).padStart(2, '0')} ${showHourAndMinute &&
-              `${parts[3]}:${parts[4]}`}`;
-          }
-          return `${year}-${String(month).padStart(2, '0')}-
-          01 ${showHourAndMinute && `${parts[3]}:${parts[4]}`}`;
-        }
-        if ((month >= 13 || month < 1) && isNaN(day)) {
-          return `${year}-${stringCurrentMonth}-
-          ${stringCurrentDate} ${showHourAndMinute && `${parts[3]}:${parts[4]}`}`;
-        }
-        if (!month && !day) {
-          return `${year}-01-01 ${showHourAndMinute && `${parts[3]}:${parts[4]}`}`;
-        }
-      }
-      if (str.length >= 1 && str.length <= 8) {
-        year = fullValidYear(str.slice(0, 4));
-        month = str.slice(4, 6);
-        day = Number(str.slice(6, 8));
-        if (str.length === 5 && Number(month) < 1) {
-          return `${year}-${stringCurrentMonth}-
-          ${stringCurrentDate} ${showHourAndMinute && `${parts[3]}:${parts[4]}`}`;
-        }
-        if ((str.length === 6 && Number(month) < 1)) {
-          return `${year}-${stringCurrentMonth}-
-          ${stringCurrentDate} ${showHourAndMinute && `${parts[3]}:${parts[4]}`}`;
-        }
-        if ((str.length === 7)) {
-          if (!isValidDay(day)) {
-            return `${year}-${String(isValidMonth(month)).padStart(2, '0')}-
-            ${stringCurrentDate} ${showHourAndMinute && `${parts[3]}:${parts[4]}`}`;
-          }
-          return `${year}-${String(isValidMonth(month)).padStart(2, '0')}-
-          ${String(day).padStart(2, '0')} ${showHourAndMinute && `${parts[3]}:${parts[4]}`}`;
-        }
-        if (str.length === 8) {
-          if (!isValidDay(day)) {
-            return `${isCurrentYear(year, month, day)}-
-            ${String(isValidMonth(month)).padStart(2, '0')}
-            -${stringCurrentDate} ${showHourAndMinute && `${parts[3]}:${parts[4]}`}`;
-          }
-          return `${isCurrentYear(year, month, day)}-
-          ${String(isValidMonth(month)).padStart(2, '0')}
-          -${String(day).padStart(2, '0')} ${showHourAndMinute && `${parts[3]}:${parts[4]}`}`;
-        }
-        if (Number(month) >= 1 && Number(month) <= 12 && isValidDay(day)) {
-          return `${year}-${month.padStart(2, '0')}-
-          ${String(day).padStart(2, '0')} ${showHourAndMinute && `${parts[3]}:${parts[4]}`}`;
-        }
-        return `${year}-
-        ${month ? month.padStart(2, '0') : '01'}-
-        ${day ?
-          String(day).padStart(2, '0') :
-          '01'} ${showHourAndMinute && `${parts[3]}:${parts[4]}`}`;
-      }
-      return `${currentYear}/${stringCurrentMonth}/${stringCurrentDate}`;
-    }
-    if (this.state.localeFormat === DATE_FORMATS.US) {
-      if (hasSpecial) {
-        month = Number(parts[0]);
-        day = Number(parts[1]);
-        year = fullValidYear(parts[2]);
-        if (month >= 1 && month <= 12 && isValidDay(day)) {
-          return `${month}/${day}/${year} ${showHourAndMinute && `${parts[3]}:${parts[4]}`}`;
-        }
-      }
-      if (str.length >= 1 && str.length <= 8) {
-        month = Number(str.slice(0, 2));
-        day = Number(str.slice(2, 4));
-        year = fullValidYear(str.slice(4, str.length));
-        if (month >= 1 && month <= 12) {
-          if (isValidDay(day)) {
-            return `${month}/${day}/${year} ${showHourAndMinute && `${parts[3]}:${parts[4]}`}`;
-          }
-          if (!day) {
-            return `${month}/1/
-            ${year} ${showHourAndMinute && `${parts[3]}:${parts[4]}`}`;
-          }
-          return `${month}/${day}/
-          ${year} ${showHourAndMinute && `${parts[3]}:${parts[4]}`}`;
-        }
-      }
-      return `${currentMonth}/
-      ${currentDate}/
-      ${currentYear} ${showHourAndMinute && `${parts[3]}:${parts[4]}`}`;
-    }
-    if (this.state.localeFormat === DATE_FORMATS.European ||
-      this.state.localeFormat === DATE_FORMATS.Germany_Russia_etc) {
-      if (hasSpecial) {
-        month = parts[1];
-        year = fullValidYear(parts[2]);
-        if (isValidDay(parts[0]) && Number(month) >= 1 && Number(month) <= 12) {
-          return `${Number(parts[0])}${delimiter}${Number(month)}
-          ${delimiter}${year} ${showHourAndMinute && `${parts[3]}:${parts[4]}`}`;
-        }
-        return `${currentDate}${delimiter}${currentMonth}
-        ${delimiter}${currentYear} ${showHourAndMinute && `${parts[3]}:${parts[4]}`}`;
-      }
-      if (str.length >= 1 && str.length <= 8) {
-        day = Number(str.slice(0, 2));
-        const monthStr = str.slice(2, 4);
-        month = isValidMonth(monthStr);
-        const yearStr = str.slice(4, str.length);
-        year = fullValidYear(yearStr);
-        if (isValidDay(day)) {
-          return `${Number(day)}${delimiter}${Number(month)}
-          ${delimiter}${year} ${showHourAndMinute && `${parts[3]}:${parts[4]}`}`;
-        }
-      }
-      return `${currentDate}${delimiter}${currentMonth}
-      ${delimiter}${currentYear} ${showHourAndMinute && `${parts[3]}:${parts[4]}`}`;
-    }
   }
 
   focus = () => {
--- src/util/index.js
+++ src/util/index.js
@@ -1,17 +1,14 @@
 import moment from 'moment';
 
-export const currentDate = moment().date();
-export const currentMonth = moment().month() + 1;
-export const currentYear = moment().year();
-export const nowTime = moment().format('HH:mm');
-export const stringCurrentDate = String(currentDate).padStart(2, '0');
-export const stringCurrentMonth = String(currentMonth).padStart(2, '0');
-
 export const DATE_FORMATS = {
   ISO: 'YYYY-MM-DD',
+  ISOAndTime: 'YYYY-MM-DD HH:mm',
   US: 'M/D/YYYY',
+  USAndTime: 'M/D/YYYY HH:mm',
   European: 'DD/MM/YYYY',
+  EuropeanAndTime: 'DD/MM/YYYY HH:mm',
   Germany_Russia_etc: 'DD.MM.YYYY',
+  Germany_Russia_etcAndTime: 'DD.MM.YYYY HH:mm',
 };
 
 const defaultDisabledTime = {
@@ -115,118 +112,4 @@ export function formatDate(value, format) {
   }
 
   return value.format(format);
-}
-
-export function formatDateLocal(formatStr, format) {
-  const str = formatStr || '';
-  let cleanStr;
-  switch (format) {
-    case DATE_FORMATS.ISO:
-      cleanStr = str.replace(/[^0-9]+/g, '-');
-      return cleanStr.split('-').filter(Boolean).map(String);
-    case DATE_FORMATS.US:
-    case DATE_FORMATS.European:
-      cleanStr = str.replace(/[^0-9]+/g, '/');
-      return cleanStr.split('/').filter(Boolean).map(String);
-    case DATE_FORMATS.Germany_Russia_etc:
-      cleanStr = str.replace(/[^0-9]+/g, '.');
-      return cleanStr.split('.').filter(Boolean).map(String);
-    default:
-      return [];
-  }
-}
-
-export function hasSpecialChar(str) {
-  const matches = str.match(/[^0-9]/g);
-  return matches ? matches.length : 0;
-}
-
-export function isValidMonth(monthStr) {
-  if (typeof monthStr === 'undefined' || monthStr === null) return currentMonth;
-  if (!/^\d+$/.test(Number(monthStr))) return currentMonth;
-  const month = Number(monthStr);
-  if (month >= 1 && month <= 12) {
-    return monthStr;
-  }
-  return currentMonth;
-}
-
-export function isValidDay(dayStr) {
-  if (!/^\d+$/.test(dayStr)) return false;
-  const day = Number(dayStr);
-  if ([1, 3, 5, 7, 8, 10, 12].includes(currentMonth)) {
-    return day >= 1 && day <= 31;
-  }
-  if ([4, 6, 9, 11].includes(currentMonth)) {
-    return day >= 1 && day <= 30;
-  }
-  if (currentMonth === 2) {
-    const isLeapYear = (year) => {
-      return (year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0);
-    };
-    const year = currentYear;
-    if (isLeapYear(year)) {
-      return day >= 1 && day <= 29;
-    }
-    return day >= 1 && day <= 28;
-  }
-}
-
-export function fullValidYear(yearStr) {
-  const year = yearStr;
-  if (!year || isNaN(year)) return currentYear;
-  if (year.length === 2) {
-    if (Number(year) >= 0 && Number(year) < 69) {
-      return year ? `20${year}` : currentYear;
-    } else if (Number(year) >= 69 && Number(year) < 100) {
-      return year ? `19${year}` : currentYear;
-    }
-  }
-  if (year.length === 4) {
-    return year;
-  }
-  return year ? year.padStart(4, '0') : currentYear;
-}
-
-export function getDateFormatByStr(str, format) {
-  const cananderStr = str || '';
-  const parts = cananderStr.split('/');
-  if (parts.length !== 3) return format;
-  const delimiter = format === DATE_FORMATS.European ? '/' : '.';
-  if (format === DATE_FORMATS.ISO) {
-    return DATE_FORMATS.ISO;
-  }
-  if (format === DATE_FORMATS.US) {
-    return DATE_FORMATS.US;
-  }
-  if ((format === DATE_FORMATS.European) || (format === DATE_FORMATS.Germany_Russia_etc)) {
-    const [day, month] = parts;
-    const dayLen = day.length;
-    const monthLen = month.length;
-    if (dayLen === 2 && monthLen === 2) {
-      return `DD${delimiter}MM${delimiter}YYYY`;
-    } else if (dayLen === 2 && monthLen === 1) {
-      return `DD${delimiter}M${delimiter}YYYY`;
-    } else if (dayLen === 1 && monthLen === 2) {
-      return `D${delimiter}MM${delimiter}YYYY`;
-    } else if (dayLen === 1 && monthLen === 1) {
-      return `D${delimiter}M${delimiter}YYYY`;
-    }
-  }
-  return format;
-}
-
-export function isCurrentYear(year, month, day) {
-  return (
-    Number(month) >= 1 &&
-    Number(month) <= 12 &&
-    Number(day) >= 1 &&
-    Number(day) <= 31
-  )
-    ? year
-    : currentYear;
-}
-
-export function isShowTimePicker(cananderStr) {
-  return `${cananderStr} `;
 }
