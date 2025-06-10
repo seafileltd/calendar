1cc42c482cbfa8904d97b0cdf1dee69d922b8fe9
GongFlying <150640661+gzcqqqqqqqq1@users.noreply.github.com>
2025-06-10 09:59:32 +0800

    Updates

--- examples/custom-clear-icon.js
+++ examples/custom-clear-icon.js
@@ -1780,8 +1780,7 @@ var _initialiseProps = function _initialiseProps() {
     var parts = Object(__WEBPACK_IMPORTED_MODULE_9__util__["d" /* formatDateLocal */])(cananderStr, _this2.state.localeFormat);
     // 没有内容，合法并直接退出
     if (!str) {
-      onChange(null);
-      _this2.onClear();
+      onChange(__WEBPACK_IMPORTED_MODULE_8_dayjs___default()());
       _this2.setState({ str: '' });
       return;
     }
