54d7048ad0eb7546acbf85756553b7a778a56e4e
GongFlying <150640661+gzcqqqqqqqq1@users.noreply.github.com>
2025-07-08 10:21:36 +0800

    Updates

--- examples/week.js
+++ examples/week.js
@@ -3545,8 +3545,9 @@ var _initialiseProps = function _initialiseProps() {
     // 没有内容，合法并直接退出
 
     if (!str || !calendarStr) {
+      console.log(123);
       onChange(null);
-      _this2.onClear();
+      // this.onClear();
       return;
     }
     var parsed = __WEBPACK_IMPORTED_MODULE_8_dayjs___default()(calendarStr, format[0]);
